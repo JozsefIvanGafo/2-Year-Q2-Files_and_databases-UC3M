79ed76fa4679f303368d4e17e44dea4ff89d8508
József <100456709@alumnos.uc3m.es>
2023-03-15 10:42:09 +0100

    Co-authored-by: marcosgova777@gmail.com <marcosgova777@gmail.com>

--- Queries.sql
+++ Queries.sql
@@ -11,18 +11,35 @@ select performer,count('x') "#total band songs" from (select pair, performer fro
 NATURAL JOIN (select pair, title from tracks) D
 group by performer fetch next 50 rows only;
 
---##############################################################################################
+--Query owned song performances
+select band,count('x') "#total band performances" from (select distinct songwriter, songtitle from performances) E
+NATURAL JOIN (select musician songwriter, band from involvement) F where band = 'Rici'
+group by band fetch next 50 rows only;
+--Query total number of performances
+select performer,count('x') "total band performings" from (select distinct performer,songtitle from performances) where performer = 'Rici' group by performer fetch next 50 rows only;
 
---Performances:
-+--doubts/temporary:
+
+
+---prev(without distinct)
 
 --Query owned song performances
 select band,count('x') "#total band performances" from (select songwriter, songtitle from performances) E
 NATURAL JOIN (select musician songwriter, band from involvement) F where band = 'Rici'
 group by band fetch next 50 rows only;
 --------------------------------------
---Query total number of performances
+
 select performer,count('x') "total band performings" from performances where performer = 'Rici' group by performer fetch next 50 rows only;
+
+
+--##############################################################################################
+--definitive 
+
+--##############################################################################################
+
+
+--Performances:
++--doubts/temporary:
+
 
 
 -- example
@@ -32,19 +49,28 @@ FROM table1
 
 
 
-
-select (count(select band,count('x') "#original songs" from (select musician passport,band from involvement) A 
-NATURAL JOIN (select writer passport, title from songs) B 
-group by band)
-/
-count(select performer band,count('x') "#total band songs" from (select pair, performer from albums) C
-NATURAL JOIN (select pair, title from tracks) D
-group by performer)
+--test
+select band, "% of of recorded tracks" ((
+    (select original_songs from (select band,count('x') original_songs from (select musician passport,band from involvement) A NATURAL JOIN (select writer passport, title from songs) B  group by band))
+    /
+    (select total_band_songs from (select performer band,count('x') total_band_songs from (select pair, performer from albums) C NATURAL JOIN (select pair, title from tracks) D group by performer))
+    )*100)
 from 
-(select performer band,count('x') "#total band songs" from (select pair, performer from albums) C
+(select band from (select performer band,count('x') total_band_songs from (select pair, performer from albums) C
 NATURAL JOIN (select pair, title from tracks) D
-group by performer))*1000fetch next 50 rows only;
-
+group by performer)) 
+fetch next 50 rows only;
+  ------------------------another test  
+select 
+(select "#original songs" from (select band,count('x') "#original songs" from (select musician passport,band from involvement) A NATURAL JOIN (select writer passport, title from songs) B  group by band))
+as Query1,
+(select "#total band songs" from (select performer band,count('x') "#total band songs" from (select pair, performer from albums) C NATURAL JOIN (select pair, title from tracks) D group by performer))
+as Query2,(
+    (select "#original songs" from (select band,count('x') "#original songs" from (select musician passport,band from involvement) A NATURAL JOIN (select writer passport, title from songs) B  group by band))
+    / 
+    (select "#total band songs" from (select performer band,count('x') "#total band songs" from (select pair, performer from albums) C NATURAL JOIN (select pair, title from tracks) D group by performer))
+) as  per_rec_songs,
+from  
 
 
 
@@ -82,9 +108,9 @@ where performer = 'Rici' group by performer,songtitle fetch next 300 rows only;
 
 --possible to remove:
 
---Query number of performed own tracks per band:
+----Query number of performed own tracks per band:
 select performer,count('x') "#original songs performed" from (select songwriter writer, performer from performances) E
 NATURAL JOIN (select writer, title from songs) F
 group by performer fetch next 50 rows only;
---
+----
 
